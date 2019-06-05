446b822b66c21d36ebbfaaf9d58395fdae81afcf
neverdieone <altiore@mail.ru>
2019-06-05 16:19:59 +0300

    Add many stars in random places.

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
 import time
 import curses
 import asyncio
+import random
+
+TIC_TIMEOUT = 0.1
+SYMBOLS = '+*.:'
+STARS_AMOUNT = 200
 
 
-async def blink(canvas, row, column, symbol='*'):
+async def blink(canvas, symbol):
+    max_x, max_y = canvas.getmaxyx()
+    x, y = random.randint(1, max_x - 2), random.randint(1, max_y - 2)
+
     while True:
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        canvas.addstr(row + 1, column, '1')
-        await asyncio.sleep(0)
+        canvas.addstr(x, y, symbol, curses.A_DIM)
+        canvas.addstr(1, 1, '1')
+        for _ in range(20):
+            await asyncio.sleep(0)
 
-        canvas.addstr(row, column, symbol)
-        canvas.addstr(row + 1, column, '2')
-        await asyncio.sleep(0)
+        canvas.addstr(x, y, symbol)
+        canvas.addstr(1, 1, '2')
+        for _ in range(3):
+            await asyncio.sleep(0)
 
-        canvas.addstr(row, column, symbol, curses.A_BOLD)
-        canvas.addstr(row + 1, column, '3')
-        await asyncio.sleep(0)
+        canvas.addstr(x, y, symbol, curses.A_BOLD)
+        canvas.addstr(1, 1, '3')
+        for _ in range(5):
+            await asyncio.sleep(0)
 
-        canvas.addstr(row, column, symbol)
-        canvas.addstr(row + 1, column, '4')
-        await asyncio.sleep(0)
+        canvas.addstr(x, y, symbol)
+        canvas.addstr(1, 1, '4')
+        for _ in range(3):
+            await asyncio.sleep(0)
 
 
-def draw(canvas, row, column):
+def draw(canvas):
     canvas.border()
     curses.curs_set(False)
 
-    coroutine = blink(canvas, row, column)
+    coroutines = [blink(canvas, random.choice(SYMBOLS)) for _ in range(STARS_AMOUNT)]
+
     while True:
-        coroutine.send(None)
+        for coroutine in coroutines:
+            coroutine.send(None)
         canvas.refresh()
-        time.sleep(1)
+        time.sleep(TIC_TIMEOUT)
+
+
+def main():
+    curses.update_lines_cols()
+    curses.wrapper(draw)
 
 
 if __name__ == '__main__':
-    curses.update_lines_cols()
-    curses.wrapper(draw, 5, 20)
+    main()
