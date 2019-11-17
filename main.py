725a6b40c3225a8676d2ecdbf7408b80a90c6aa8
neverdie <altiore@mail.ru>
2019-11-17 16:51:15 +0300

    New try to write async code.

--- main.py
+++ main.py
@@ -1,56 +1,58 @@
 import time
 import curses
 import asyncio
+import curses
 import random
 
 TIC_TIMEOUT = 0.1
-SYMBOLS = '+*.:'
+SYMBOLS = 'ABCD'  # на буквах лучше видно мигание если делать не в репле
 STARS_AMOUNT = 200
 
 
-async def blink(canvas, symbol):
-    max_x, max_y = canvas.getmaxyx()
-    x, y = random.randint(1, max_x - 2), random.randint(1, max_y - 2)
-
-    while True:
-        canvas.addstr(x, y, symbol, curses.A_DIM)
-        canvas.addstr(1, 1, '1')
-        for _ in range(20):
-            await asyncio.sleep(0)
-
-        canvas.addstr(x, y, symbol)
-        canvas.addstr(1, 1, '2')
-        for _ in range(3):
-            await asyncio.sleep(0)
-
-        canvas.addstr(x, y, symbol, curses.A_BOLD)
-        canvas.addstr(1, 1, '3')
-        for _ in range(5):
-            await asyncio.sleep(0)
-
-        canvas.addstr(x, y, symbol)
-        canvas.addstr(1, 1, '4')
-        for _ in range(3):
-            await asyncio.sleep(0)
+def get_random_xy(max_x, max_y):
+    return random.randint(1, max_x-2), random.randint(1, max_y-2)
 
 
 def draw(canvas):
-    canvas.border()
     curses.curs_set(False)
+    canvas.border()
 
-    coroutines = [blink(canvas, random.choice(SYMBOLS)) for _ in range(STARS_AMOUNT)]
+    max_x, max_y = canvas.getmaxyx()
+    coroutines = [blink(canvas, *get_random_xy(max_x, max_y), random.choice(SYMBOLS)) for _ in range(STARS_AMOUNT)]
 
     while True:
         for coroutine in coroutines:
-            coroutine.send(None)
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
+        if len(coroutines) == 0:
+            break
+
         canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
 
-def main():
-    curses.update_lines_cols()
-    curses.wrapper(draw)
+async def blink(canvas, row, column, symbol='A'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        for _ in range(20):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        for _ in range(5):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
 
 if __name__ == '__main__':
-    main()
+    curses.update_lines_cols()
+    curses.wrapper(draw)
+
